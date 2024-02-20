d77769bd6e925629f3a3f5ec2a555c80ef871a5e
RichardRodi <RichardmRodi@gmail.com>
2024-02-20 14:17:10 -0500

    Added switch statements for favorite subjects in school

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in School?");
+            string studentAnswer = Console.ReadLine();
+            switch (studentAnswer) 
+            {
+                case "Math":
+                    Console.WriteLine("Math is Stupid");
+                    break;
+                case "History":
+                    Console.WriteLine("History never repeats itself");
+                    break;
+                case "English":
+                    Console.WriteLine("English I speak it good");
+                    break;
+                case "Physical Education":
+                case "PE":
+                case "Phys Ed":
+                    Console.WriteLine("Physical Education is the only right answer");
+                    break;
+                case "Home Economics":
+                case "Home Ec":
+                    Console.WriteLine("Home Economics, who can afford a home anyway");
+                    break;
+                default:
+                    Console.WriteLine("This is not a subject");
+                    break;
+            }
         }
     }
 }
